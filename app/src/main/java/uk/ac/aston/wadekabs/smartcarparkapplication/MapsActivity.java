5ef707805fe9ea9809e2106e66c5dcc3a735d4b1
Bhalchandra Wadekar <BhalchandraSW@gmail.com>
2017-02-14 17:33:49 +0000

    Added new icon for app from http://www.iconsdb.com/icons/preview/navy-blue/car-xxl.png and changed app name to ParkUp

--- app/src/main/java/uk/ac/aston/wadekabs/smartcarparkapplication/MapsActivity.java
+++ app/src/main/java/uk/ac/aston/wadekabs/smartcarparkapplication/MapsActivity.java
@@ -39,6 +39,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -93,6 +94,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void setMap(GoogleMap mMap) {
         this.mMap = mMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        UiSettings settings = mMap.getUiSettings();
+        settings.setZoomControlsEnabled(true);
+        settings.setIndoorLevelPickerEnabled(false);
+        settings.setMyLocationButtonEnabled(true);
+        settings.setMapToolbarEnabled(false);
+
         updateUI();
     }
 
@@ -277,8 +297,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     protected void stopLocationUpdates() {
-        LocationServices.FusedLocationApi.removeLocationUpdates(
-                mGoogleApiClient, this);
+        if (mGoogleApiClient.isConnected())
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    mGoogleApiClient, this);
     }
 
     @Override
