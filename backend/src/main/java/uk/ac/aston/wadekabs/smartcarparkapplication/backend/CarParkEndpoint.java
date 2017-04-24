cb6d30fada2842fd7672d583a6512791908d438d
Bhalchandra Wadekar <BhalchandraSW@gmail.com>
2017-04-24 16:53:10 +0100

    Clean Up

--- backend/src/main/java/uk/ac/aston/wadekabs/smartcarparkapplication/backend/CarParkEndpoint.java
+++ backend/src/main/java/uk/ac/aston/wadekabs/smartcarparkapplication/backend/CarParkEndpoint.java
@@ -88,16 +88,4 @@ public class CarParkEndpoint {
 
         return carParkList;
     }
-
-    /**
-     * A simple endpoint method that takes a name and says Hi back
-     */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
-
-        MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
-        return response;
-    }
 }
